dd6720637901dcd85e60bb9563c36a1ee2cfd057
IskahnX <davidecroce10@gmail.com>
2022-01-31 16:39:32 +0100

    added the data and added more contacts

--- js/script.js
+++ js/script.js
@@ -1,1 +1,47 @@
 console.log('VUE OK', Vue);
+
+Vue.config.devtool = true;
+
+const boolzapp = new Vue ({
+    el: '#root',
+    data: {
+        user: {
+          name: 'Ezreal',
+          avatar: '_io'
+        },
+        contacts: [
+          {
+            name: 'Leona',
+            avatar: '_1',
+          },
+          {
+            name: 'Lux',
+            avatar: '_2',
+          },
+          {
+            name: 'Wukong',
+            avatar: '_3',
+          },
+          {
+            name: 'Professor Graves',
+            avatar: '_4',
+          },
+          {
+            name: 'Caitlyn',
+            avatar: '_5',
+          },
+          {
+            name: 'Katarina',
+            avatar:'_6',
+          },
+          {
+            name: 'Darius',
+            avatar: '_7',
+          },
+          {
+            name: 'Jayce',
+            avatar: '_8',
+          }
+        ]
+      }
+});
